2466610f3f4e4be79082a4e2e1da3d1d00358342
zohir <jahirraihan500@gmail.com>
2025-02-06 18:24:29 +0600

    host

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    outDir: 'dist', // Ensure the build output is in the 'dist' folder
+  },
 })
